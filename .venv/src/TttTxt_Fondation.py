1e8bc7eb97191d23c1efe7bda50ba0b632f6c17e
Martin <martin.geris@alumni.univ-avignon.fr>
2025-09-26 14:33:53 +0200

    Ajout TttTxt_Fondation
    Création d'un programme spécifique pour le pré-traitement du corpus "Fondation". Ajouts réels : Suppression des numéros de pages, Suppression des phrases coupées.

---
+++ .venv/src/TttTxt_Fondation.py
@@ -0,0 +1,83 @@
+import fitz
+import re
+import os
+
+
+
+#-----------------------------------FONCTIONS---------------------------------------
+
+
+
+def pdf_to_text(pdf_path):
+    """Extract text from a PDF file."""
+    text = ""
+    with fitz.open(pdf_path) as doc:
+        for page in doc:
+            text += page.get_textpage().extractText()
+            text += " -_ENDPAGE_- "
+    return text
+
+def supprCutSentences(texte):
+    matches = list(re.finditer(r"( -_ENDPAGE_- )", texte))
+    for match in reversed(matches):             #On commence par la fin pour ne pas perturber les indices
+        avant = texte[:match.start()]
+        apres = texte[match.end():]
+        last_ponct = max(avant.rfind('.'), avant.rfind('!'), avant.rfind('?'))
+        if last_ponct != -1:
+            avant = avant[:last_ponct+1]        #On supprime après la dernière ponctuation
+        else:
+            avant = ""
+
+        texte = avant + " -_ENDPAGE_- " + apres #On garde les marqueurs pour traiter les phrases post marqueurs
+
+    matches = list(re.finditer(r"( -_ENDPAGE_- )", texte))
+    for match in reversed(matches):                       #On commence par le début pour ne pas perturber les indices
+        avant = texte[:match.start()]
+        apres = texte[match.end():]
+        if len(apres)!=0:
+            premiere_lettre = next((c for c in apres if c.isalpha()))
+            if not premiere_lettre.isupper():              #Si pas de majuscule juste après le marqueur
+                apres_poncts = [apres.find('.'), apres.find('!'), apres.find('?')]
+                if not all(x == -1 for x in apres_poncts):
+                    apres_poncts = [x for x in apres_poncts if x != -1]
+                first_ponct = min(apres_poncts)
+                if first_ponct != -1:
+                    apres = apres[first_ponct+1:]   #On supprime avant la première ponctuation
+                else:
+                    apres = ""
+
+        texte = avant + apres
+
+    return texte
+
+# FAIRE FONCTION SUPPRIMER NUMEROS DE CHAPITER ET DE PARTIE
+
+# FAIRE FONCTION GERER DIALOGUES
+
+# FAIRE FONCTION RECUPERER MOTS NON LU PAR FITZ
+
+# FAIRE FONCTION RECUPERER MOTS INCOPLETS (voulu par l'auteur) 
+
+
+
+#------------------------------TRAITEMENT DU TEXTE---------------------------------
+
+
+
+os.chdir('..')
+os.chdir('..')
+corpus = pdf_to_text("./Corpus/Corpus_ASIMOV/Fondation_sample.pdf")
+
+corpus = re.sub(r'\ufffd\s*\d+\s*\ufffd', ' ', corpus)  #Supprime les numéros de pages
+corpus = supprCutSentences(corpus)                      #Supprime les phrases coupées
+corpus = re.sub(r'\s+', ' ', corpus)                    #Remplace '/n', '/t', ' ' isolé comme groupé par un espace
+
+
+
+#-----------------------------------ECRITURE---------------------------------------
+
+
+
+f = open('fichier.txt', 'w', encoding="utf-8")
+f.write(corpus)
+f.close()
