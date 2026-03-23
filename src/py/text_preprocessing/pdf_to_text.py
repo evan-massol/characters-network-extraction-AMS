a3446727053fb34895fc50e530864ef871d12124
Evan MASSOL <evan.massol@alumni.univ-avignon.fr>
2026-03-23 12:59:25 +0100

    Improves score with more entries in antidict + rename fondation_sample to pdf_to_text

---
+++ src/py/text_preprocessing/pdf_to_text.py
@@ -0,0 +1,22 @@
+import re
+import os
+from py.text_preprocessing.utils import pdf_to_text, supprPartsTitle, supprChapterNum, supprCutSentences
+
+#-----------------------------PRETRAITEMENT DU TEXTE-------------------------------
+
+
+for pdf in os.listdir("./pdf/"):
+    if pdf.endswith(".pdf"):
+        print(f"Processing {pdf}...")
+        corpus = pdf_to_text(os.path.join("./pdf/", pdf)) 
+        corpus = re.sub(r'\ufffd\s*\d+\s*\ufffd', ' ', corpus)  #Supprime les numéros de pages
+        corpus = supprPartsTitle(corpus)                        #Supprime les titres
+        corpus = supprChapterNum(corpus)                        #Supprime les numéros de chapitres
+        corpus = supprCutSentences(corpus)                      #Supprime les phrases coupées
+        corpus = re.sub(r'\s+', ' ', corpus)                    #Remplace '/n', '/t', ' ' isolé comme groupé par un espace
+
+        #-----------------------------------ECRITURE---------------------------------------
+
+        output_path = os.path.join('./txt/corpus_classique/', pdf.replace('.pdf', '.txt'))
+        with open(output_path, 'w', encoding="utf-8") as f:
+            f.write(corpus)
